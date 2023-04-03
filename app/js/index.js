9603f4f064933bbc105d20a596eb6df44a5b2c98
TJ Egan <tw15egan@gmail.com>
2023-04-03 20:29:28 +0200

    update styles

--- app/js/index.js
+++ app/js/index.js
@@ -1,29 +1,27 @@
-import Clipboard from "clipboard";
+import Clipboard from 'clipboard';
 
-const emailButton = new Clipboard("#email");
-const tooltip = document.querySelector(".tooltip");
+const emailButton = new Clipboard('#email');
+const tooltip = document.querySelector('.tooltip');
 
-emailButton.on("success", evt => {
-  tooltip.classList.remove("hidden");
+emailButton.on('success', (evt) => {
+  tooltip.classList.remove('hidden');
   setTimeout(() => {
-    tooltip.classList.add("hidden");
+    tooltip.classList.add('hidden');
   }, 1500);
 });
 
-emailButton.on("error", e => {
-  console.error("whoops");
+emailButton.on('error', (e) => {
+  console.error('whoops');
 });
 
-const triggers = document.querySelectorAll(".social svg");
-const social = document.querySelector(".social");
-const highlight = document.createElement("span");
+const triggers = document.querySelectorAll('.social svg');
+const social = document.querySelector('.social');
+const highlight = document.createElement('span');
 const initialPos = triggers[0].getBoundingClientRect();
 
-highlight.classList.add("box");
-highlight.classList.add("hidden");
-highlight.style.transform = `translate(${initialPos.left}px, ${
-  initialPos.top
-}px) scale(1.4)`;
+highlight.classList.add('box');
+highlight.classList.add('hidden');
+highlight.style.transform = `translate(${initialPos.left}px, ${initialPos.top}px) scale(1.4)`;
 
 document.body.append(highlight);
 
@@ -31,19 +29,17 @@ function highlightIcon() {
   const rect = this.getBoundingClientRect();
   const offsetTop = rect.top;
 
-  highlight.style.transform = `translate(${
-    rect.left
-  }px, ${offsetTop}px) scale(1.4)`;
+  highlight.style.transform = `translate(${rect.left}px, ${offsetTop}px) scale(1.4)`;
 
-  if (highlight.classList.contains("hidden")) {
-    highlight.classList.remove("hidden");
+  if (highlight.classList.contains('hidden')) {
+    highlight.classList.remove('hidden');
   }
 }
 
-triggers.forEach(icon => {
-  icon.addEventListener("mouseenter", highlightIcon);
+triggers.forEach((icon) => {
+  icon.addEventListener('mouseenter', highlightIcon);
 });
 
-social.addEventListener("mouseleave", () => {
-  highlight.classList.add("hidden");
+social.addEventListener('mouseleave', () => {
+  highlight.classList.add('hidden');
 });
